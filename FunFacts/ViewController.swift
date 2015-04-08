b7aac3453ded2c54c7d83f597b4baef33cc1a270
David Ladowitz <david@ladowitz.com>
2015-04-08 15:08:08 -0700

    creating a model

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let factBook = FactBook()
+    @IBOutlet weak var funFactLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        funFactLabel.text = factBook.randomFact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +24,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFunFact() {
-        println("You pressed Me")
+        funFactLabel.text = factBook.randomFact()
     }
 
 }
--- Scratchpad.playground/section-1.swift
+++ Scratchpad.playground/section-1.swift
@@ -16,3 +16,35 @@ if (optionalString != nil) {
 //
 //optionalString = nil
 //optionalString = "bye"
+
+let factsArray = [
+    "Montpelier, Vermont is the only state capital without a McDonalds.",
+    "A cow gives nearly 200,000 glasses of milk in her lifetime.",
+    "Cuba is the only island in the Caribbean to have a railroad.",
+    "Earthworms have 5 hearts.",
+    "People say “bless you” when you sneeze because your heart stops for a millisecond."
+]
+
+factsArray.count
+
+
+struct FactBook {
+    let funFacts = [
+        "Ants stretch when they wake up in the morning.",
+        "Ostriches can run faster than horses.",
+        "Olympic gold medals are actually made mostly of silver.",
+        "You are born with 300 bones; by the time you are an adult you will have 206.",
+        "It takes about 8 minutes for light from the Sun to reach Earth.",
+        "Some bamboo plants can grow almost a meter in just one day.",
+        "The state of Florida is bigger than England.",
+        "Some penguins can leap 2-3 meters out of the water.",
+        "On average, it takes 66 days to form a new habit.",
+        "Mammoths still walked the earth when the Great Pyramid was being built." ]
+}
+
+var factBook = FactBook()
+factBook.funFacts
+
+
+
+
