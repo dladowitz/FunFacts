31f20b8802c34673179a711a52c5f35923f9b96d
David Ladowitz <david@ladowitz.com>
2015-04-08 10:41:23 -0700

    intial commit for funfacts app

---
+++ Scratchpad.playground/section-1.swift
@@ -0,0 +1,18 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var someVariable = "aVariable"
+let someConstant = "aConstant"
+
+var fruitsArray = ["apple", "pear"]
+//fruitsArray.append(10)
+
+var optionalString: String? = "help"
+
+if (optionalString != nil) {
+    println(optionalString!)
+}
+//
+//optionalString = nil
+//optionalString = "bye"
